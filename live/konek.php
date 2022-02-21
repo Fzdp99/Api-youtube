0fa24c90b9407cc95af601f2f45c631154719e21
Fatwa Zuhri <97175664+Fzdp99@users.noreply.github.com>
2022-02-21 14:06:02 +0700

    Update konek.php

--- live/konek.php
+++ live/konek.php
@@ -4,9 +4,9 @@
         $sc = $_POST['sc'];
         $cr = str_replace(" ","%",$sc);
         if($sc == ""){
-                curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&maxResults=50&q=news&type=video&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&maxResults=50&q=news&type=video&key=YOUR_KEY');
         }else{
-                curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&maxResults=50&q='.$cr.'&type=video&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&maxResults=50&q='.$cr.'&type=video&key=YOUR_KEY');
         }
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $data = curl_exec($curl);
