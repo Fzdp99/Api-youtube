88700d234a71866d923582d7c3dbf523497ade85
Fatwa Zuhri <97175664+Fzdp99@users.noreply.github.com>
2022-02-21 14:10:57 +0700

    Update konek.php

--- trending/konek.php
+++ trending/konek.php
@@ -1,7 +1,7 @@
 <?php
         error_reporting(0);
         $curl = curl_init();
-        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=ID&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=ID&key=YOUR_KEY');
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
         $data = curl_exec($curl);
         curl_close($curl);
