175c8f6f3641f54e9b0bd53c3bdb2e3413077e74
Fatwa Zuhri <97175664+Fzdp99@users.noreply.github.com>
2022-02-21 14:07:54 +0700

    Update konek.php

--- t_api/konek.php
+++ t_api/konek.php
@@ -4,47 +4,47 @@
         $ktg = $_POST['ktgi'];
         switch ($ktg) {
                 case 1:
-                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%facebook&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%facebook&key=YOUR_KEY');
 
                         break;
                 case 2:
-                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%instagram&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%instagram&key=YOUR_KEY');
 
                         break;
                 case 3:
-                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%youtube&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%youtube&key=YOUR_KEY');
 
                         break;
                 case 4:
-                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%twitter&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%twitter&key=YOUR_KEY');
 
                         break;
                 case 5:
-                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%whatsapp&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%whatsapp&key=YOUR_KEY');
 
                         break;
                 case 6:
-                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%telegram&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%telegram&key=YOUR_KEY');
 
                         break;
                 case 7:
-                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%line&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%line&key=YOUR_KEY');
 
                         break;
                 case 8:
-                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%google%map&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%google%map&key=YOUR_KEY');
 
                         break;
                 case 9:
-                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%indodax&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%indodax&key=YOUR_KEY');
                         
                         break;
                 case 10:
-                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%binance&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=api%binance&key=YOUR_KEY');
 
                         break;
                 default:
-                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Application%20Programming%20Interface&key=AIzaSyDNk2bibYkXkfr--dNPHyTf-yjRTi1ubd0');
+                        curl_setopt($curl, CURLOPT_URL, 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=Application%20Programming%20Interface&key=YOUR_KEY');
                         
                         break;
         }
